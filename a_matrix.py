972d47561c44faad7181e8da9a94fd5fae9c8c03
rizzopp <paolo.rizzo@outlook.com>
2018-06-05 17:09:54 +0200

    aoo

---
+++ a_matrix.py
@@ -0,0 +1,14 @@
+"""
+Path Planning & Optimization for IROS 2018 Competition
+DSE Group 25
+"""
+
+wps = [1,2]
+n_wps = 4
+n_derivative = 4
+
+def make_matrix_a():
+    for way_point in range(0,n_wps):
+        for derivative in range(0,n_derivative):
+            if
+    pass
